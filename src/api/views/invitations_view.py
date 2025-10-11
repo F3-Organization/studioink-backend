0159aa6dd774e8332545080420bbb708a124b11b
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-11 18:03:47 -0400

    refactor: update InvitationArtistViewSet to use correct serializers

--- src/api/views/invitations_view.py
+++ src/api/views/invitations_view.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,33 +7,48 @@ from rest_framework.viewsets import ViewSet
 from api.models.invitation import Invitation
 from api.permissions.is_studio_owner import IsStudioOwnerOrReadOnly
 from api.permissions.is_subscription_studio import IsStudioSubscription
-from api.serializers import InvitationArtistSerializer
+from api.serializers import InvitationInputArtistSerializer, InvitationSerializer
+from api.services.invitation_service import InvitationService
 from api.tasks.send_email import send_invitation_email_task
 
 
 class InvitationArtistViewSet(ViewSet):
-    serializer_class = InvitationArtistSerializer
+    serializer_class = InvitationInputArtistSerializer
     permission_classes = [
         IsAuthenticated,
         IsStudioOwnerOrReadOnly,
         IsStudioSubscription,
     ]
 
+    @extend_schema(
+        request=None,
+        responses=InvitationSerializer(many=True),
+        tags=["Invitations"],
+    )
     def list(self, request, *args, **kwargs):
         queryset = Invitation.objects.filter(studio__owner=request.user)
-        serializer = self.serializer_class(queryset, many=True)
+        serializer = InvitationSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(
+        request=InvitationInputArtistSerializer,
+        responses=InvitationSerializer,
+        tags=["Invitations"],
+    )
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         invitation = self.perform_create(serializer)
         return Response(
-            InvitationArtistSerializer(invitation).data,
+            InvitationSerializer(invitation).data,
             status=status.HTTP_201_CREATED,
         )
 
     def perform_create(self, serializer):
-        invitation = serializer.save()
+        invitation = InvitationService().create_invitation(
+            serializer.validated_data["email"], self.context["request"]
+        )
         send_invitation_email_task(invitation).delay()
         return invitation
