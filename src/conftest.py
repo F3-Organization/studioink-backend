4db87145675143b8f9dab7299dd6f487e6067d8a
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-12 15:09:42 -0400

    feat: add pytest fixture to configure database settings fron .env

---
+++ src/conftest.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+import pytest
+from django.conf import settings
+from dotenv import load_dotenv
+
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(env_path, override=True)
+
+
+@pytest.fixture(autouse=True)
+def django_db_setup():
+    """Override database settings for tests from environment variables"""
+    settings.DATABASES["default"]["HOST"] = os.getenv("POSTGRES_HOST")
+    settings.DATABASES["default"]["PORT"] = os.getenv("POSTGRES_PORT")
+    settings.DATABASES["default"]["NAME"] = os.getenv("POSTGRES_NAME")
+    settings.DATABASES["default"]["USER"] = os.getenv("POSTGRES_USER")
+    settings.DATABASES["default"]["PASSWORD"] = os.getenv("POSTGRES_PASSWORD")
