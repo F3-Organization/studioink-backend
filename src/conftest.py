e40d7b2204507db55db7229cac63fbfa87a85f5e
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-13 22:15:39 -0400

    fix: set default database host to localhost in test setup

--- src/conftest.py
+++ src/conftest.py
@@ -12,7 +12,7 @@ load_dotenv(env_path, override=True)
 @pytest.fixture(autouse=True)
 def django_db_setup():
     """Override database settings for tests from environment variables"""
-    settings.DATABASES["default"]["HOST"] = os.getenv("POSTGRES_HOST")
+    settings.DATABASES["default"]["HOST"] = "localhost"
     settings.DATABASES["default"]["PORT"] = os.getenv("POSTGRES_PORT")
     settings.DATABASES["default"]["NAME"] = os.getenv("POSTGRES_NAME")
     settings.DATABASES["default"]["USER"] = os.getenv("POSTGRES_USER")
