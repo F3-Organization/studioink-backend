42f933d414ceefd4a261ce60979743cdedecc2e5
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-14 15:56:26 -0400

    test: add integration tests for create_invitation method

--- src/api/tests/integration/test_invitation_service.py
+++ src/api/tests/integration/test_invitation_service.py
@@ -1,4 +1,5 @@
 import pytest
+from rest_framework.exceptions import APIException
 
 from api.services.invitation_service import InvitationService
 
@@ -8,3 +9,11 @@ def test_create_invitation_successfully(email, registered_user):
     service = InvitationService()
     invitation = service.create_invitation(email, registered_user)
     assert invitation.email == email
+
+
+@pytest.mark.django_db
+def test_create_invitation_with_an_error(email, fake_user):
+    service = InvitationService()
+    with pytest.raises(APIException) as excinfo:
+        service.create_invitation(email, fake_user)
+    assert str(excinfo.value) == "Studio not found for the current user."
