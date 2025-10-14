807619f8a5a51e06b803f8f908cf3b0c84b379f9
Felipe Oliveira <flipeaz342@gmail.com>
2025-10-14 11:45:33 -0400

    feat: add UserFactory for generating user test data

---
+++ src/api/tests/factories.py
@@ -0,0 +1,16 @@
+import factory
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        skip_postgeneration_save = True
+
+    username = factory.Faker("user_name")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    email = factory.Faker("email")
+    is_active = True
+    is_staff = False
+    is_superuser = False
